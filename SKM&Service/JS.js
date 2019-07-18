de80c515d9984ec4b1e9e4fcf1dedccc01374f05
KimKeonWoo <50134433+lieion@users.noreply.github.com>
2019-07-18 09:41:30 +0900

    Add files via upload

--- SKM&Service/JS.js
+++ SKM&Service/JS.js
@@ -39,6 +39,48 @@ var viz;
       
 	}
 
+
+
+
+  function draw_by_menu() {
+        //config 형식에 맞춰어서 작성 
+        center_of_graph=jsoned3;
+        var config = {
+            container_id: "viz",
+            server_url: "bolt://localhost:7687",
+            server_user: "neo4j",
+            server_password: "1234", // 자신의 비밀번호를 입력 
+            arrow: true,
+            labels: { 
+               "Product": {
+                   "caption": "title",
+                   "size": "pid",
+                   "community":"community"
+                   //"sizeCypher": "MATCH (n) WHERE id(n) = {id} MATCH (n)-[r]-() RETURN sum(r.weight) AS c"
+                }
+              },
+              relationships: {
+                 "CO_PURCHASE":{
+                     "caption": false,
+                      "thickness": "weight"
+                }
+              },    
+              // pid가 jsoned3 인 노드로 부터 1~ num 거리에 있는 것들을 최대 limit_num만큼 출력 
+              initial_cypher: "MATCH p=()-[r:CO_PURCHASE*1.."+num+"]->(product : Product {pid : '"+ center_of_graph +"'}) RETURN p LIMIT "+limit_num+""
+
+                    
+
+        };
+        console.log(num);
+        console.log(jsoned3);
+        console.log(limit_num);
+        
+      viz = new NeoVis.default(config);
+      hide_Info();
+      hide_Length();
+      viz.render();
+      
+  }
     /*
       이 함수는 검색 기능에 사용 된다 .
       edge를 출력 안하는 것이 특징이다. 
@@ -96,6 +138,8 @@ var viz;
     function hide_Length_Smoothly(){
       $(".jb-length").slideUp(300,"linear");
     }
+
+   
 
 
 
