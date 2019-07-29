52eba3cff8b5275ecfccb85013990c724e199fd3
KimKeonWoo <50134433+lieion@users.noreply.github.com>
2019-07-29 16:59:10 +0900

    Add files via upload

--- SKM&Service/JS.js
+++ SKM&Service/JS.js
@@ -112,8 +112,6 @@ var viz;
               },    
               // Product title에 검색어가 있는 product를 개수 제한 없이 가져오는 역할 
               initial_cypher: "MATCH (product1:Product) WHERE product1.title CONTAINS \'"+search_text+"\' RETURN product1"
-
-
         };
 
       cyphermethod= "MATCH (product1:Product) WHERE product1.title CONTAINS \'"+search_text+"\' RETURN product1";
@@ -222,5 +220,21 @@ var viz;
   }
 
 
-
-
+function sample() {
+        
+        var background = document.getElementById('jb-content').style.background;
+        if(background == "") {
+            document.getElementById('jb-content').style.background = "#fff"; 
+        }
+            
+        html2canvas(document.getElementById('jb-content'), {
+            useCORS: true, // 다른사이트의리소스가있을때활성화(그러나...Access-Control-Allow-Origin 필요)
+            onrendered: function(canvas) {
+                canvas.toBlob(function(blob) {
+                    saveAs(blob, 'download.png');
+                });
+                
+                // $("#test").html('<img src=' + canvas.toDataURL("image/png") + '>');
+            }
+        });
+    }
